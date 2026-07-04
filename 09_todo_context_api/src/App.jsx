e1222c427f2e2abcd08accd7c5d713a9d1437ffc
yuvrajsingh <yuvrajsingh6554@gmail.com>
2026-07-04 16:16:24 +0530

    feat: add initial setup for React app with Vite and Tailwind CSS
    - Added React and Vite configuration files.
    - Created main entry point for the application.
    - Included Tailwind CSS for styling.
    - Added SVG assets for React and Vite logos.

---
+++ 09_todo_context_api/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <h1 className='text-3xl font-bold underline bg-white'>helo</h1>
+    </>
+  )
+}
+
+export default App
